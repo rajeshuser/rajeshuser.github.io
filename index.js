e8fd97d6431616b0a1eb7c2ad7334129e0b41960
virtual_bot <rajeshdyawarkonda123@gmail.com>
2023-02-25 18:49:01 +0530

    updated resume

--- index.js
+++ index.js
@@ -3,3 +3,15 @@ document.querySelector("#menu-icon").addEventListener("click", (event) => {
 	const display = navOptions.style.display;
 	navOptions.style.display = display == "flex" ? "none" : "flex";
 });
+
+document.getElementById("resume-link-1").addEventListener("click", downloadFile);
+
+document.getElementById("resume-link-2").addEventListener("click", downloadFile);
+
+function downloadFile() {
+	let link = document.createElement("a");
+	link.href = "Rajesh_Dyawarkonda_Resume.pdf";
+	link.download = "Rajesh_Dyawarkonda_Resume.pdf";
+	link.click();
+	link.remove();
+}
